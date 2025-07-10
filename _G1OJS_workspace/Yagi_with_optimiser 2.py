bcf3b0c50a0867d04a47593e4483156b95c962a8
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-07-10 23:00:33 +0100

    Tidy up and add MIT license to each file

--- _G1OJS_workspace/Yagi_with_optimiser 2.py
+++ _G1OJS_workspace/Yagi_with_optimiser 2.py
@@ -31,45 +31,26 @@ from nec_lib.nec_wrapper import NECModel
 from nec_lib import geometry_builder
 from nec_lib import wire_viewer
 
-def build_antenna(model, A_mm, B_mm, C_mm, D_mm, main_wire_diameter_mm):
-
-    E_mm  = B_mm + C_mm + D_mm
+def build_antenna(model, l0_mm, l1_mm, l2_mm, dy1_mm, dy2_mm, w0d_mm, w1d_mm, w2d_mm):
 
     model.start_geometry()
     antenna_components = geometry_builder.components()
 
-    front = antenna_components.wire_Z(length_mm = A_mm, wire_diameter_mm = main_wire_diameter_mm)
-    front.rotate_ZtoX()
-    front.translate(dx_mm = 0, dy_mm = E_mm , dz_mm = 0)
+    ref = antenna_components.wire_Z(length_mm = l0_mm, wire_diameter_mm = w0d_mm)
+    ref.rotate_ZtoX()
+    driv = antenna_components.wire_Z(length_mm = l1_mm, wire_diameter_mm = w1d_mm)
+    driv.rotate_ZtoX()
+    driv.translate(dx_m = 0, dy_mm = dy1_mm, dz_m = 0)
+    d1 = antenna_components.wire_Z(length_mm = l2_mm, wire_diameter_mm = w2d_mm)
+    d1.rotate_ZtoX()
+    d1.translate(dx_m = 0, dy_mm = dy1_mm + dy2_mm, dz_m = 0)
+    
+    model.place_feed(driv, feed_alpha_object = 0.5)
 
-    front_left = antenna_components.wire_Z(length_mm = B_mm, wire_diameter_mm = main_wire_diameter_mm)
-    front_left.rotate_ZtoX()
-    front_left.rotate_around_Z(90)
-    front_left.translate(dx_mm = -A_mm/2, dy_mm = B_mm/2 + C_mm + D_mm, dz_mm = 0)
+    model.add(ref)
+    model.add(driv)
+    model.add(d1)
 
-    front_right = antenna_components.copy_of(front_left)
-    front_right.translate(dx_mm = A_mm, dy_mm = 0, dz_mm = 0)
-
-    rear = antenna_components.copy_of(front)
-    rear.translate(dx_mm = 0, dy_mm = -E_mm, dz_mm = 0)
-
-    rear_left = antenna_components.wire_Z(length_mm =D_mm, wire_diameter_mm = main_wire_diameter_mm)
-    rear_left.rotate_ZtoX()
-    rear_left.rotate_around_Z(90)
-    rear_left.translate(dx_mm = -A_mm/2, dy_mm = D_mm/2, dz_mm = 0)
-
-    rear_right = antenna_components.copy_of(rear_left)
-    rear_right.translate(dx_mm = A_mm, dy_mm = 0, dz_mm = 0)
- 
-    model.place_feed(front, feed_alpha_object = 0.5)
-
-    model.add(front)
-    model.add(front_left)
-    model.add(front_right)
-    model.add(rear)
-    model.add(rear_left)
-    model.add(rear_right)    
- 
     return model
 
 def cost_function(model):
@@ -81,7 +62,6 @@ def cost_function(model):
 def optimise():
     from nec_lib.optimisers import RandomOptimiser
     param_init = params
-    bounds = {"l0_mm":(800,1200),"l1_mm":(800,1200),"l2_mm":(800,1200),"y1_mm":(100,310),"y2_mm":(320,800)}
     best_params, best_info = RandomOptimiser(build_antenna, param_init, cost_function,
                                              max_iter=1000, bounds = bounds).optimise(model, verbose=False)
 
@@ -93,17 +73,21 @@ model.set_frequency(MHz = 144.2)
 model.set_gain_point(azimuth = 90, elevation = 3)
 model.set_ground(eps_r = 11, sigma = 0.01, origin_height_m = 8.0)
 
-params = {'A_mm': 750, 'B_mm': 100, 'C_mm': 30, 'D_mm': 140, 'main_wire_diameter_mm': 10}
+params = {'l0_mm': 708.92, 'l1_mm': 1021.76, 'l2_mm': 939.93, 'dy1_mm': 154.03, 'dy2_mm': 208.08, 'w0d_mm': 8.64, 'w1d_mm': 11.25, 'w2d_mm': 7.85}
+bounds = {"l0_mm":(600,1200),"l1_mm":(800,1200),"l2_mm":(800,1200),"dy1_mm":(10,1000),"dy2_mm":(10,1000)}
+
 # Optimiser Results (copy and paste into your antenna file for reference)
-# [] INITIAL: VSWR:1.60 Gain:11.25 with {'A_mm': '750.00', 'B_mm': '100.00', 'C_mm': '30.00', 'D_mm': '140.00', 'main_wire_diameter_mm': '10.00'}
-# []   FINAL: VSWR:1.57 Gain:12.00 with {'A_mm': '727.87', 'B_mm': '98.98', 'C_mm': '26.05', 'D_mm': '133.48', 'main_wire_diameter_mm': '9.72'}
+# [] INITIAL: VSWR:2.31 Gain:12.00 with {'l0_mm': 800.01, 'l1_mm': 1030.42, 'l2_mm': 946.23, 'dy1_mm': 172.44, 'dy2_mm': 189.56, 'w0d_mm': 9.06, 'w1d_mm': 10.97, 'w2d_mm': 7.50}
+# []   FINAL: VSWR:2.11 Gain:12.17 with {'l0_mm': 708.92, 'l1_mm': 1021.76, 'l2_mm': 939.93, 'dy1_mm': 154.03, 'dy2_mm': 208.08, 'w0d_mm': 8.64, 'w1d_mm': 11.25, 'w2d_mm': 7.85}
+# []   FINAL: VSWR:2.08 Gain:12.23 with {'l0_mm': 669.80, 'l1_mm': 1015.67, 'l2_mm': 939.54, 'dy1_mm': 151.21, 'dy2_mm': 216.39, 'w0d_mm': 8.79, 'w1d_mm': 11.54, 'w2d_mm': 7.54}
 
 
 model = build_antenna(model, **params)
 model.write_nec()
-wire_viewer.view_nec_input(model.nec_in, model.EX_TAG, title = "Moxon")
-
+wire_viewer.view_nec_input(model.nec_in, model.EX_TAG, title = "Yagi")
 optimise()
+
+
 print("Done")
 
 
